78dfb1951217296c2b17e77a314af321af316278
noah <noah.thiering@web.de>
2023-01-29 14:10:26 +0100

    [feature] init commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Analytics from "./components/analytics/Analytics";
+import Exercises from "./components/exercises/Exercises";
+import Settings from "./components/settings/Settings";
+import Workouts from "./components/workouts/Workouts";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Exercises />
+      <Workouts />
+      <Analytics />
+      <Settings />
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
